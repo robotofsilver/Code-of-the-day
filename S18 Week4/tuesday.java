75a65375a3b12d28af556820cc4a1834cb064b91
Rushabh J Shah <rushabhshah130@gmail.com>
2018-02-20 15:16:00 -0800

    Rushabh's Method

--- S18 Week4/tuesday.java
+++ S18 Week4/tuesday.java
@@ -2,12 +2,29 @@ public class tuesdays{
   
   
   public static void main(String arg[]){
-    String str = "A Man, A Plan, A Canal-Panama!";
-    
-    
-    
+    String checking = "A Man, A Plan, A Canal-Panama!";
+    tuesdays t=new tuesdays();
+    if(t.palindrome){
+      System.out.println("Palindrome");
+    }else{
+      System.out.println("Not Palindrome");
   }
-
+  public boolean palindromeCheck(String checking) { // Rushabh's Method
+		boolean validity=false;//predicting that the string is not palindrome
+		int counter=0;//Intializing a counter
+		for(int i=0;i<checking.length();i++) {
+			for(int j=checking.length()-1;j>=0;j--) {
+				if(checking.charAt(i) == checking.charAt(j)) {
+					counter++;//Checking howmany characters are matching
+					break;
+				}
+			}
+		}
+		if(counter==checking.length()) { // if the counter and the length of the string matches 
+			validity=true;//makes the boolean true
+		}
+		return validity;
+	}
 
 
 
