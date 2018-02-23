89ade15e72e754cc104345fdba1d36be86bfe8e2
DanielRodriguez98 <35978469+DanielRodriguez98@users.noreply.github.com>
2018-02-23 14:51:22 -0800

    Friday.java

--- S18 Week4/Friday.java
+++ S18 Week4/Friday.java
@@ -16,6 +16,25 @@ public friday {
         }
         return str2.toString();
     }
+                          
+     public static String stackMethod(String str)//Daniel;s Method
+    {
+        Stack stack1 = new Stack();
+
+        for (int i = 0; i <= str.length() -1 ; i++) 
+        {
+            stack1.push(str.charAt(i));
+        }
+
+        String bob = "";
+
+        while(!(stack1.isEmpty()))
+        {
+            bob += stack1.pop();
+        }
+
+        return bob;
+    }
 
 
 }
