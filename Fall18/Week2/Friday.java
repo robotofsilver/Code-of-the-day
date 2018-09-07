e62487248a5ef2f2890a57d38dc6d8248a9629fc
Kevin Ramirez <kevinramirez266@yahoo.com>
2018-09-07 13:37:19 -0700

    Create Friday.java

---
+++ Fall18/Week2/Friday.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+public class Friday {
+  Random rng = new Random();
+  public static void main(String[] args) {
+    int[] age_array = new int[1000];
+    
+    for (int current_index = 0; current_index < 1000; current_index++)
+    {
+      age_array[current_index] = rng.nextInt(16) + 5;
+    }
+    
+    //Create an method which takes in an array of ages and returns a sorted array.
+  }
+}
