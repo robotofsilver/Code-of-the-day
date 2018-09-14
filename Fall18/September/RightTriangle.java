7dfcb000bec21e41783ef19e6cf8d1bfe991bda3
Kevin Ramirez <kevinramirez266@yahoo.com>
2018-09-13 20:35:57 -0700

    Create RightTriangle.java

---
+++ Fall18/September/RightTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class RightTriangle {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);                         //Creates a Scanner object.
+    System.out.print("How many rows will make the triangle? ");     //Request user input for an integer for triangle size.
+    int numOfRows = input.nextInt();                                //Use Scanner object to call method which takes input from keyboard.
+    
+     /*   Create a method which takes in an integer value, returns nothing, and prints out a right trangle made of stars.
+     *    Example:
+     *    How many rows will make the triangle? 5
+     *    *
+     *    **
+     *    ***
+     *    ****
+     *    *****
+     */
+  }
+  
+}
