849d1181710a10c0b183de32be7401c0628502af
Kevin Ramirez <kevinramirez266@yahoo.com>
2018-11-09 10:14:15 -0800

    Create MyStack.java

---
+++ Fall18/November/MyStack.java
@@ -0,0 +1,48 @@
+//Written by Martin Lennan
+class MyStack {
+    int[] stackArray;
+    int stackPointer;
+
+    public OrigStack(int size) {
+        stackArray = new int[size];
+        stackPointer = 0;
+    }
+
+    public void push(int num) {
+        if (stackPointer < stackArray.length - 1) {
+            stackArray[stackPointer] = num;
+            stackPointer++;
+        }
+    }
+
+    public int pop() {
+        if (stackPointer > 0) {
+            stackPointer--;
+            int toReturn = stackArray[stackPointer];
+            return toReturn;
+        }
+        return -999;
+    }
+
+    public int peek() {
+        if (stackPointer > 0) {
+            return stackArray[stackPointer - 1];
+        }
+        return -999;
+    }
+    
+    public boolean isEmpty() {
+        if (stackPointer == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < stackPointer; i++) {
+            s += stackArray[i] + " | ";
+        }
+        return s;
+    }
+}
