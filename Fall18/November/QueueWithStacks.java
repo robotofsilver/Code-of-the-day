d20733213762085153d92b8e1b77b06c208b2817
Martin Lennan <35021956+robotofsilver@users.noreply.github.com>
2018-11-09 12:01:26 -0800

    Update QueueWithStacks.java

--- Fall18/November/QueueWithStacks.java
+++ Fall18/November/QueueWithStacks.java
@@ -8,7 +8,7 @@ public class QueueWithStacks {
     Stacks are another data structure that follows a different convention of first in last out, where it behaves like a stack of dishes.
     Only the top most dish will be used, and the first dish in a stack is the last to be used.
     
-    For this practice, write a method named QueuePoll that returns the first item in a queue utilizing only stacks.
+    For this practice, write a class named QueuePoll that returns the first item in a queue utilizing only stacks.
     You may use array provided functionalities, but do not simply return the first index as your method, that is cheeky.
     You are permitted to use more than one stack if needed.
     We have also provided a stack class that provides the four functionalities of a stack, 
@@ -20,6 +20,74 @@ public class QueueWithStacks {
     System.out.println(frontElement);
   }
   
-  //Add your QueuePoll method below this line. Include your name above the method you added.
   
+}
+  //Add your QueuePoll class below this line. Include your name above the method you added.
+
+class QueuePoll //Martin Lennan's queue
+{
+	MyStack stackPile;
+	MyStack queuePile;
+	boolean isStack;
+	
+	public QueuePoll(int size)
+	{
+		stackPile = new stack(size);
+		queuePile = new stack(size);
+		isStack = true;
+	}
+	
+	public void enqueue(int num)
+	{
+		if (!isStack)
+		{
+			flipStacks();
+		}
+		stackPile.push(num);
+	}
+	
+	public int dequeue()
+	{
+		if (isStack)
+		{
+			flipStacks();
+		}
+		return queuePile.pop();
+	}
+	
+	public int peek()
+	{
+		if (isStack)
+		{
+			flipStacks();
+		}
+		return queuePile.peek();
+	}
+	
+	public boolean isEmpty()
+	{
+		if (isStack)
+		{
+			return stackPile.isEmpty();
+		}
+		return queuePile.isEmpty();
+	}
+	
+	private void flipStacks()
+	{
+		if (isStack)
+		{
+			while (!stackPile.isEmpty())
+			{
+				queuePile.push(stackPile.pop());
+			}
+		}
+		else
+		{
+			while (!queuePile.isEmpty())
+			{
+				stackPile.push(queuePile.pop());
+			}
+		}
+	}	
 }
